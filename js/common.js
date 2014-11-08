aeacc0384b87e5e24c5a0413201ad52f3016888f
Jason Light <rilwal2@gmail.com>
2014-11-08 14:30:52 +1300

    Sprint 2

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
 (function () {
     "use strict";
     var startupApp = angular.module('startupApp', []);
-    startupApp.controller('startupCtrl', ['$scope',
-        function ($scope) {
-            $scope.hello = "hello";
+    startupApp.filter('to_trusted', ['$sce', function($sce){
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    }]);
+    startupApp.controller('startupCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+            $scope.active="home";
+            $('#navbar >* >* >*').click(function(){
+                $scope.active=$(this).attr('data-target');
+                $http.get('templates/'+$scope.active+'.html');
+            });
+            
         }]);
 }());
